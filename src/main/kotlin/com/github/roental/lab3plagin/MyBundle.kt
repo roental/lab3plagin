9ac74f3b304a9df061621a07bb42587089c70025
GitHub Action <action@github.com>
2025-12-12 09:10:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/roental/lab3plagin/MyBundle.kt
+++ src/main/kotlin/com/github/roental/lab3plagin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.roental.lab3plagin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/roental/lab3plagin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/roental/lab3plagin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.roental.lab3plagin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.roental.lab3plagin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
